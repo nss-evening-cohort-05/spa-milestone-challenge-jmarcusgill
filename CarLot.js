3b4f364f2af8edebeaf2236b9252f9bc90bc003c
jmarcusgill <jmarcusgill@gmail.com>
2017-04-03 14:56:14 -0500

    added activate events function

--- CarLot.js
+++ CarLot.js
@@ -4,14 +4,12 @@ var CarLot = (function () {
   return {
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
-
-      inventoryLoader.addEventListener("load", function () {
-        inventory = JSON.parse(this.responseText).cars;
-        // console.log(inventory)
-        CarLot.populatePage(inventory);
-      });
       inventoryLoader.open("GET", "inventory.json");
       inventoryLoader.send();
+      inventoryLoader.addEventListener("load", function () {
+        inventory = JSON.parse(this.responseText).cars;
+        CarLot.populatePage(inventory);
+      });
     }
   };
 

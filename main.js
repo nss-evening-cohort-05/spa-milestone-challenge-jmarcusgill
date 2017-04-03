7123c0c2253cf976d1c8ba26d785c27366377f2d
jmarcusgill <jmarcusgill@gmail.com>
2017-04-03 14:56:58 -0500

    added description class

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ oldCarLot.populatePage = function (inventory) {
   var carBuilder = "";
   for (var i = 0; i < inventory.length; i++) {
     var currentcar = inventory[i];
-    console.log("this is current car", currentcar)
-    carBuilder += `<div class="col-md-4 carCard">`;
+    // console.log("this is current car", currentcar)
+    carBuilder += `<div class="col-sm-6 col-md-4 carCard">`;
     carBuilder += `<div class="thumbnail">`;
     carBuilder += `<img class="imgSize" src="${currentcar.url}">`
     carBuilder += `<div class="caption">`;
     carBuilder += `<h3> ${currentcar.make} </h3>`;
     carBuilder += `<h3> ${currentcar.model} </h3>`;
     carBuilder += `<h3> ${currentcar.year} </h3>`;
-    carBuilder += `<p> ${currentcar.description} </p>`;
+    carBuilder += `<div class="description"><p> ${currentcar.description} </p></div>`;
     carBuilder += `<h4> $${currentcar.price} </h4>`;
     carBuilder += `</div></div></div>`;
   }
   carOutput.innerHTML += carBuilder;
   // Now that the DOM is loaded, establish all the event listeners needed
-  // CarLot.activateEvents();
+  oldCarLot.activateEvents();
 }
 
 // Load the inventory and send a callback function to be
